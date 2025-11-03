2f5a3a1324328790c1ac087a32acfd5603bc68fc
rwanda95do <dlsdnr2379@naver.com>
2025-11-03 19:21:58 +0900

    merge445454

--- src/main/java/edu/example/bts/controller/DeployRequestReportController.java
+++ src/main/java/edu/example/bts/controller/DeployRequestReportController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import edu.example.bts.domain.deployRequest.DeployFormDevRepoDTO;
@@ -44,4 +45,6 @@ public class DeployRequestReportController {
 		model.addAttribute("commitFilesList", commitFilesList);
 		return "/deploy/deployRequestReportView";
 	}
+	
+	
 }
